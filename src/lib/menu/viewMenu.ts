066e3ae005cec546e32e78fdbc5581b29ac4efb3
Darxoon <darxoon@outlook.com>
2023-01-09 17:15:03 +0100

    Make "View all Descriptions" menu show all descriptions of child types

--- src/lib/menu/viewMenu.ts
+++ src/lib/menu/viewMenu.ts
@@ -1,9 +1,11 @@
 import type EditorStrip from "$lib/editor/EditorStrip.svelte"
+import { DataTypeExtension, dataTypeExtensions } from "$lib/editor/fileEditor/dataTypeExtensions"
 import { showModal } from "$lib/modal/modal"
 import TextAlert from "$lib/modal/TextAlert.svelte"
 import DescriptionViewer from "$lib/modals/DescriptionViewer.svelte"
 import NoteViewer from "$lib/modals/NoteViewer.svelte"
 import { globalEditorStrip } from "$lib/stores"
+import { DataType } from "paper-mario-elfs/elfBinary"
 import { FILE_TYPES } from "paper-mario-elfs/fileTypes"
 
 let editorStrip: EditorStrip
@@ -41,10 +43,31 @@ function viewAllDescriptions() {
 	let	tab = editorStrip.activeTab()
 	let dataType = tab.properties.dataType
 	
-	if (Object.entries(FILE_TYPES[dataType].metadata).length > 0) {
-		showModal(DescriptionViewer, {
-			typeMetadata: FILE_TYPES[dataType].metadata,
-		})
+	// the map that every metadata will be stored in
+	let allMetadata = new Map()
+	
+	// add metadata of current data type
+	let mainMetadata = FILE_TYPES[dataType].metadata
+	allMetadata.set(dataType, mainMetadata)
+	
+	// add metadata of child types if it's a group type like data_ui or data_btl
+	if (dataTypeExtensions(DataTypeExtension.HasComplexEditor, dataType)) {
+		let childTypes = dataTypeExtensions(DataTypeExtension.ComplexEditorCategory, dataType)
+		
+		for (const [name, { dataType }] of Object.entries(childTypes)) {
+			allMetadata.set(dataType, FILE_TYPES[dataType].metadata)
+		}
+	}
+	
+	// maplink header
+	if (dataType == DataType.Maplink) {
+		allMetadata.set(DataType.MaplinkHeader, FILE_TYPES[DataType.MaplinkHeader].metadata)
+	}
+	
+	// TODO: check if anything contains any more child types
+	
+	if (Object.entries(mainMetadata).length > 0 || allMetadata.size > 1) {
+		showModal(DescriptionViewer, { allMetadata })
 	} else {
 		showModal(TextAlert, {
 			title: "No Descriptions",
